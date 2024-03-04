911210a5f74fbf5bb9d513100ba5f543d5ab7720
arkngel212 <143550869+arkngel212@users.noreply.github.com>
2024-03-04 09:54:38 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let who = ["El gato", "La oruga", "El vecino", "La IA"];
+let action = ["corrompió", "inutilizó", "editó", "borró"];
+let what = ["el Javascript", "el CSS", "el HTML"];
+let when = [
+  "de la web del cliente",
+  "del ejercicio de clase",
+  "mientras yo cocinaba",
+  "porque no le ofrecí comida",
+  "mientras dormía"
+];
+
+function generateExcuse() {
+  let randomwho = who[Math.floor(Math.random() * who.length)];
+  let randomaction = action[Math.floor(Math.random() * action.length)];
+  let randomwhat = what[Math.floor(Math.random() * what.length)];
+  let randomwhen = when[Math.floor(Math.random() * when.length)];
+
+  return randomwho + " " + randomaction + " " + randomwhat + " " + randomwhen;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuse = generateExcuse();
+  console.log(excuse);
+  document.querySelector("#excuse").innerHTML = excuse;
 };
